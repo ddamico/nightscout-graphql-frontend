5bbff852475f9bdc50be7de396ea04fff98a8277
David D'Amico <human@ddamico.org>
2019-01-13 21:02:20 -0500

    adds skeleton for nsdashboard component tests

---
+++ tests/unit/nighscoutdashboard.spec.js
@@ -0,0 +1,10 @@
+import { shallowMount } from '@vue/test-utils';
+import NightscoutDashboard from '@/components/NightscoutDashboard';
+
+describe('NightscoutDashboard.vue', () => {
+    test('should mount for testing', () => {
+        const wrapper = shallowMount(NightscoutDashboard, {
+
+        });
+    })
+});
